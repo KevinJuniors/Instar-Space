7915357150796d6d23a2bd2b6182dc72e0f1b694
DESKTOP-U172KAU\home <su960618@gmail.com>
2022-03-24 13:45:52 +0900

    [2022-03-24] 프리즈마 스튜디오

--- server.js
+++ server.js
@@ -21,14 +21,15 @@ const typeDefs = gql`
 
     type Mutation {
         createMovie(title: String!, year: Int!, genre: String): Movie
-        deleteMovie(id: String!): Boolean
+        deleteMovie(id: Int!): Movie
+        updateMovie(id: Int! year: Int!): Movie
     }
 `;
 
 const resolvers = {
     Query: {
         movies: () => client.movie.findMany(),
-        movie: (_, {id}) => ({ title: "Hello", year: 2022 }),
+        movie: (_, {id}) => client.movie.findUnique({ where: {id} }),
     },
 
     Mutation: {
@@ -41,10 +42,14 @@ const resolvers = {
             },
         }),
 
-        deleteMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
+        deleteMovie: (_, { id }) => client.movie.delete({
+            where: {id}
+        }),
+
+        updateMovie: (_, { id, year }) => client.movie.update({
+            where: {id},
+            data: {year}
+        })
     },
 };
 
