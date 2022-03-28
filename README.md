33ab5b0922b0a7073dfecaeba1f66c0ce039095a
DESKTOP-U172KAU\home <su960618@gmail.com>
2022-03-29 00:01:09 +0900

    [2022-03-29 00:00] schema.prisma 오타 수정 및 사용자 프로필 참조기능 구현

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-## Instar-Projcet 한 줄 소개
-인스타그램 카피 프로젝트 Github 저장소입니다.  
+# Instaclone
 
-## Skills  
-React, Express Module, Prisma  
-
-## History  
-2022-01-07 Instar-Projcet Github 저장소 생성  
-2022-01-08 GraphQL Server 구축  
-2022-01-09 Apollo Server 구축  
-2022-01-17 Babal 설치 및 적용  
-2022-01-20 Prisma 설치 및 Project 적용    
-2022-03-23 프리즈마 마이그레이션 & 클라이언트    
+Instaclone Backend.
--- user/user.mutations.js
+++ user/user.mutations.js
@@ -8,6 +8,7 @@ export default {
             _, 
             { firstName, lastName, username, email, password }
         ) => {
+            try {
             // DB에 입력된 Data와 사용자가 전달 해준 userName, Email Data와 대조
             const existingUser = await client.user.findFirst ({
                 where: {
@@ -17,10 +18,16 @@ export default {
                     ],
                 },
             });
-            console.log(existingUser);
+            
+            // 이미 DB에 등록된 userName이나 passWord를 사용할 경우 출력될 오류메세지
+            if (existingUser) {
+                throw new Error("해당 사용자이름 또는 비밀번호는 이미 사용중입니다.");
+            }
 
             // 사용자의 비밀번호를 Hash function를 통헤 Mapping
             const uglyPassword = await bcrypt.hash(password, 10);
+            
+            // 사용자 데이터를 저장
             const user = client.user.create({data: {
                 username,
                 email,
@@ -29,9 +36,12 @@ export default {
                 password: uglyPassword
             }});
 
-
+            // 사용자 데이터를 반환
             return user;
-            // 사용자 데이터를 저장하고 반환
+            
+            } catch(e) {
+                return e;
+            }
         },
     },
 };
